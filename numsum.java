286b0756d66574a76c39f8498b1998c7e0be8102
Vibhor Kumar <139122820+vib123vib@users.noreply.github.com>
2025-04-06 12:34:30 +0530

    Update numsum.java
    This is an example code to sum numbers using function sum() and accessing it through object in main class.

--- numsum.java
+++ numsum.java
@@ -1,1 +1,12 @@
-hello 
+import java.util.*
+public class Add
+  {
+    public static void main(String []args)
+    {
+      static int sum(int a, int b){ return a+b;}
+      Add a = new Add();
+      //an example of use, always use object a to access sum function.
+      System.out.println(a.sum(56,87));
+    } 
+    
+  }
